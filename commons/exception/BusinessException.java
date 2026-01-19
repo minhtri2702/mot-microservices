8bb4cb3592ad07a6588c4f9c2ec29a7a17677e0f
minhtri2702 <minhtrihoang97@gmail.com>
2026-01-19 15:53:47 +0700

    firt commit

---
+++ commons/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.demo.commons.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
+
+
